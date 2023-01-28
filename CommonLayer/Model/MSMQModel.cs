27c8dda074d79e809fb7ec9e00906049714b23fd
pareeksachin77 <pareeksachin77@gmail.com>
2023-01-28 15:14:03 +0530

    [sachin: 27-EncryptDecrptPass]

--- CommonLayer/Model/MSMQModel.cs
+++ CommonLayer/Model/MSMQModel.cs
@@ -30,7 +30,7 @@ namespace CommonLayer.Model
             var message = mq.EndReceive(e.AsyncResult);
             string token = message.Body.ToString();
             string subject = "Test Message";
-            string body = token;
+            string body = $"Fundoo Notes Reset Password: <a href=http://localhost:4200/resetPassword/{token}> Click Here</a>";
             var smtp = new SmtpClient("smtp.gmail.com")
             {
                 Port = 587,
